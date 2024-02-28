3ac9c441f051b2b9efe0eb867805069b20d7ee93
hokagedevu1 <hokagedevu1@gmail.com>
2024-02-28 15:43:33 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Summer
         </a>
       </header>
     </div>
